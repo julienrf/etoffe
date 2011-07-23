3f1b533e28800ff3252a6208e1c59124bed1cd83
Julien Richard-Foy <julien@richard-foy.fr>
2011-07-23 14:41:58 +0200

    Fix Java API

--- src/main/scala/etoffe/java/JEtoffe.scala
+++ src/main/scala/etoffe/java/JEtoffe.scala
@@ -8,13 +8,15 @@ import collection.JavaConversions
  * Convenient object to help poor Java programmers to use Etoffe
  */
 object JEtoffe {
-  def parse(text: String, indexGen: IndexGenerator) = Etoffe.parse(text, indexGen)
+  def parse(text: String) = Etoffe.parse(text)
   
   def render(text: String, indexGen: IndexGenerator) = Etoffe.render(text, indexGen)
   
   def renderParagraphs(text: String, indexGen: IndexGenerator): EtoffeResult = {
-    val (paragraphs: JIterable[String], footnotes: JIterable[String]) = Etoffe.renderParagraphs(text, indexGen)
-    new EtoffeResult(paragraphs, footnotes)
+    val (paragraphs, footnotes) = Etoffe.renderParagraphs(text, indexGen)
+    new EtoffeResult(
+        JavaConversions.asJavaIterable(paragraphs.toIterable),
+        JavaConversions.asJavaIterable(footnotes.toIterable))
   }
 }
 
