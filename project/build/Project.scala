2dc83602e7fe4294ea3f0b3995d8083f9aaa14f6
Julien Richard-Foy <julien@richard-foy.fr>
2011-07-23 14:39:59 +0200

    Change compilation options

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,4 +4,5 @@ import de.element34.sbteclipsify._
 class Project(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
   override def artifactID = "etoffe"
   val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1"
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
